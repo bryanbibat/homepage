68322554cfc0f5a0b5efac433ffec858f66baac9
Bryan Bibat <bry@bryanbibat.net>
2014-02-13 06:49:20 +0800

    Update gems

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
+  set :assets_host, 'bryanbibat.net'
+  set :assets_css_compressor, :sass
+  set :assets_js_compressor, :yui
+
   register Sinatra::AssetPipeline
 
   get '/' do
